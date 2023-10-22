92a6f61920b8ab2ca923b79ffdb0cf0c32b82eda
cesther31 <estherchong731@gmail.com>
2023-10-22 12:28:06 -0400

    test

--- ClassPlus/backend/test.java
+++ ClassPlus/backend/test.java
@@ -20,12 +20,22 @@
 */
 
 import java.System.*;
-
+import java.sql.*;  
 
 public class test {
 	public static void main(String[] arg){
 
 		System.out.println("This is test");
 
+		try{  
+			Class.forName("com.mysql.jdbc.Driver");  
+			Connection con=DriverManager.getConnection("sql9sql9.freemysqlhosting.net","sql9654993","jyG6f1zdxz");  		
+			//here sonoo is database name, root is username and password  
+			Statement stmt=con.createStatement();  
+			ResultSet rs=stmt.executeQuery("select * from emp");  
+				while(rs.next())  
+					System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+					con.close();  
+		}catch(Exception e){System.out.println(e);}   
 	}
 }
