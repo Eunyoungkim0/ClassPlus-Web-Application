631067c1b3fa084cd8b9db8f928e672114c4aab1
cesther31 <estherchong731@gmail.com>
2023-10-22 12:22:52 -0400

    test

--- ClassPlus/backend/test.java
+++ ClassPlus/backend/test.java
@@ -1,5 +1,31 @@
+/*
+		Server: sql9.freemysqlhosting.net
+		Name: sql9654993
+		Username: sql9654993
+		Password: jyG6f1zdxz
+		Port number: 3306
+
+		USE sql9654993;
+
+		// TABLE LIST and detail information
+		SELECT * 
+		  FROM information_schema.TABLES
+		 WHERE TABLE_SCHEMA = 'sql9654993';
+
+		// COLUMN LIST and detail information
+		 SELECT *
+		   FROM information_schema.COLUMNS
+		  WHERE TABLE_SCHEMA = 'sql9654993';
+		--   AND TABLE_NAME = 'users';
+*/
+
+import java.System.*;
+
+
 public class test {
 	public static void main(String[] arg){
+
+		System.out.println("This is test");
 
 	}
 }
