5217f760fcfa887b5694c432a034496ef6985e1c
cesther31 <estherchong731@gmail.com>
2023-10-22 14:14:05 -0400

    mySQL drivers / test connection

--- ClassPlus/backend/test.java
+++ ClassPlus/backend/test.java
@@ -18,7 +18,9 @@
 		  WHERE TABLE_SCHEMA = 'sql9654993';
 		--   AND TABLE_NAME = 'users';
 */
-import java.sql.*;  
+import java.sql.*;
+import oracle.jdbc.driver.*; 
+import oracle.sql.*;  
 
 public class test {
 	public static void main(String[] arg){
@@ -26,8 +28,8 @@ public class test {
 		System.out.println("This is test");
 
 		try{  
-			Class.forName("com.mysql.jdbc.Driver");  
-			Connection con=DriverManager.getConnection("sql9sql9.freemysqlhosting.net","sql9654993","jyG6f1zdxz");  		
+			//Class.forName("com.mysql.jdbc.Driver");  
+			Connection con=DriverManager.getConnection("jdbc:mysql://sql9sql9.freemysqlhosting.net","sql9654993","jyG6f1zdxz");  		
 			//here sonoo is database name, root is username and password  
 			Statement stmt=con.createStatement();  
 			ResultSet rs=stmt.executeQuery("SELECT * FROM USER");  
