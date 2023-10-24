f8411103683e24bab0acee8b125589b4b2bebec8
cesther31 <estherchong731@gmail.com>
2023-10-23 20:54:36 -0400

    signup page(wip)

--- ClassPlus/backend/signup.java
+++ ClassPlus/backend/signup.java
@@ -2,5 +2,17 @@ import utils.connectDB;
 import java.sql.*;
 
 public class signup {
-    
+    private String userId = null;
+    private String userPwd = null;
+    private int unccId;
+    private String lastN;
+    private String firstN;
+    private String email;
+    private String major;
+    private String minor;
+    private ??? picture;
+    private boolean isStudent;
+    private boolean isInst;
+    private boolean isTA;
+    private Date signupDate;
 }
