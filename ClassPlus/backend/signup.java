6ff9d20d1e93737ecf021763b494c7d738ade958
cesther31 <122581154+cesther31@users.noreply.github.com>
2023-10-24 12:38:35 -0400

    Update signup.java

--- ClassPlus/backend/signup.java
+++ ClassPlus/backend/signup.java
@@ -2,17 +2,50 @@ import utils.connectDB;
 import java.sql.*;
 
 public class signup {
-    private String userId = null;
-    private String userPwd = null;
+    private String userPwd;
     private int unccId;
     private String lastN;
     private String firstN;
     private String email;
     private String major;
     private String minor;
-    private ??? picture;
+    private String picture;
     private boolean isStudent;
     private boolean isInst;
     private boolean isTA;
     private Date signupDate;
+    
+    private Connection conn;
+
+    public signup(){
+    }
+
+    /**
+     * Return false when the user Id exists
+     */
+    public boolean unccIdExists(){
+        try{
+            int unccId = 801232989;
+
+            conn = connectDB.getConnection();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM users");
+            while (rs.next()) {
+                int uid = rs.getInt("unccId");
+                if(uid == unccId){
+                    System.out.println("This unccId is already Exists.");
+                    return false;
+                }
+            }
+            System.out.println("This unccId is avaliable.");
+            return true;
+        }catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * Return false when the uncc Id exists
+     */
 }
