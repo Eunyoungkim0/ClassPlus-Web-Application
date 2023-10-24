1ebdff4ed73a8cff00b9ab5c725ea02a5cfe5ffc
cesther31 <estherchong731@gmail.com>
2023-10-24 14:27:00 -0400

    update connectDB with new server

--- ClassPlus/backend/utils/connectDB.java
+++ ClassPlus/backend/utils/connectDB.java
@@ -7,9 +7,9 @@ package utils;
 import java.sql.*;
 
 public class connectDB {
-    static String url = "jdbc:mysql://sql9.freemysqlhosting.net:3306/sql9654993";
-    static String username = "sql9654993"; // Change to your MySQL username
-    static String password = "jyG6f1zdxz"; // Change to your MySQL password
+    static String url = "jdbc:mysql://sql9.freemysqlhosting.net";
+    static String username = "sql9656322"; // Change to your MySQL username
+    static String password = "7qXDYWB5c3"; // Change to your MySQL password
 
 
     public static Connection getConnection() throws SQLException {
