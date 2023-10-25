310cb8347b787bf2a0f8af8b2d779f8d6a82319e
Eunyoung Kim <78773764+Eunyoungkim0@users.noreply.github.com>
2023-10-25 00:57:55 -0400

    changed database setting

--- ClassPlus/backend/utils/connectDB.java
+++ ClassPlus/backend/utils/connectDB.java
@@ -7,9 +7,9 @@ package utils;
 import java.sql.*;
 
 public class connectDB {
-    static String url = "jdbc:mysql://sql9.freemysqlhosting.net:3306/sql9656322";
-    static String username = "sql9656322"; // Change to your MySQL username
-    static String password = "7qXDYWB5c3"; // Change to your MySQL password
+    static String url = "jdbc:mysql://classplus.mysql.database.azure.com:3306/classplus";
+    static String username = "classplus"; // Change to your MySQL username
+    static String password = "uncc4155!"; // Change to your MySQL password
 
 
     public static Connection getConnection() throws SQLException {
