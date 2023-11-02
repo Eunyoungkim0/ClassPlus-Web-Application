3206d3738e0c44d6478d359cf29b7aacedd8c25e
cesther31 <estherchong731@gmail.com>
2023-11-02 10:25:21 -0400

    posting page (need to be designed

--- ClassPlus/js/utils.js
+++ ClassPlus/js/utils.js
@@ -9,3 +9,18 @@ $.get("searchbar.html", function(data){
 $.get("course_top.html", function(data){
     $("#courseTop-placeholder").replaceWith(data);
 });
+
+function navigateToCoursePage() {
+    window.location.href = 'MyCourse.html';
+}
+
+function studySetsFunction() {
+    window.location.href = 'course_page_study_sets.html';
+}
+function navigateToPosting() {
+    window.location.href = 'course_page_posting.html';
+}
+
+function navigateToEnroll() {
+  window.location.href = 'course_page_searchpage.html';
+}
